ac6a06faa53996fe3ebf4f9a05a9b7fd255e8554
Juri <lubonjajurgen1@gmail.com>
2024-02-06 16:32:31 +0100

    install mongoose

---
+++ mean-course-2/backend/models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+  //* title: String   CAN LEAVE ALSO LIKE THIS
+  title: {type: String, required: true},
+  content: {type: String, required: true}
+})
+
+module.exports =  mongoose.model('Post',postSchema)
