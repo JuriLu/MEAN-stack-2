fb4ac2cf81491cc3fb73200d917ac239ad14d75d
Juri <lubonjajurgen1@gmail.com>
2024-02-06 12:39:46 +0100

    Installing nodemon for constantly reading server.js file without needing to restart after changes
    Installing debug package
    
    Initial config for server.js

--- mean-course-2/server.js
+++ mean-course-2/server.js
@@ -1,8 +1,51 @@
-const http = require("http")
 const APP = require('./backend/app')  //* EXPRESS APP
-const PORT = process.env.PORT | 3000   //* CUSTOM PORT
+const http = require("http")
+const debug = require("debug")("node-angular");
+const normalizePort = val => {
+  let port = parseInt(val, 10);
 
-APP.set('port', PORT)                                                        //* SETTTING PORT
-const server = http.createServer(APP)  //* SETTING APP
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-server.listen(PORT)
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr;
+  debug("Listening on " + bind);
+};
+
+const PORT = normalizePort(process.env.PORT || "3000");
+APP.set("port", PORT);
+
+const server = http.createServer(APP);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(PORT);
