a164b884763b429463f26958ab1b2a7e1b290e4d
JurgenLubonja <jurgen.lubonja@eng.it>
2024-02-07 16:16:57 +0100

    Editing link styles

--- mean-course-2/src/app/app.module.ts
+++ mean-course-2/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatAnchor, MatButton, MatIconButton} from "@angular/material/button";
 import { HeaderComponent } from './header/header.component';
 import {MatToolbar} from "@angular/material/toolbar";
 import {
@@ -30,28 +30,29 @@ import {HttpClientModule} from "@angular/common/http";
     HeaderComponent,
     PostListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    MatCard,
-    MatFormField,
-    MatLabel,
-    MatInput,
-    MatButton,
-    MatToolbar,
-    MatAccordion,
-    MatExpansionPanel,
-    MatExpansionPanelHeader,
-    MatDivider,
-    MatCardContent,
-    MatError,
-    MatExpansionPanelActionRow,
-    MatIconButton,
-    MatIcon,
-    MatCardTitle,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        MatCard,
+        MatFormField,
+        MatLabel,
+        MatInput,
+        MatButton,
+        MatToolbar,
+        MatAccordion,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatDivider,
+        MatCardContent,
+        MatError,
+        MatExpansionPanelActionRow,
+        MatIconButton,
+        MatIcon,
+        MatCardTitle,
+        HttpClientModule,
+        MatAnchor
+    ],
   providers: [
     provideAnimationsAsync()
   ],
