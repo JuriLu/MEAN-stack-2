86ef4f78a950a22c35eb621e86af4a7b90465458
Juri <lubonjajurgen1@gmail.com>
2024-02-08 11:04:05 +0100

    Reload updated posts

--- mean-course-2/src/app/posts/services/posts.service.ts
+++ mean-course-2/src/app/posts/services/posts.service.ts
@@ -8,78 +8,83 @@ const BASE_URI: string = 'http://localhost:3000'
 const POST_ENDPOINT: string = '/api/posts'
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PostsService {
-    private posts: Post[] = []
-    private postsUpdated: Subject<Post[]> = new Subject<Post[]>()
+  private posts: Post[] = []
+  private postsUpdated: Subject<Post[]> = new Subject<Post[]>()
 
-    constructor(
-        private http: HttpClient
-    ) {
-    }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
 
-    getPosts(): void {
-        this.http.get<Post[]>('http://localhost:3000/api/posts')
-            .pipe(
-                map((postsData: Post[]) => {
-                    return postsData.map((post: any) => {
-                        return {
-                            title: post.title,
-                            content: post.content,
-                            id: post._id
-                        }
-                    })
-                })
-            )
-            .subscribe((postData: Post[]): void => {
-                this.posts = postData
-                this.postsUpdated.next([...this.posts])
-            })
-    }
-
-    getPost(id: string): Observable<Post> {
-        return this.http.get<Post>(BASE_URI + POST_ENDPOINT + `/${id}`)
-    }
-
-    addPost(inPost: Partial<Post>): void {
-        const post: Post = {
-            id: null,
-            title: inPost.title as string,
-            content: inPost.content as string
-        }
-        this.http.post<PostResponseDto>(BASE_URI + POST_ENDPOINT, post).subscribe(
-            (responseData: PostResponseDto): void => {
-                post.id = responseData.postId
-                this.posts.push(post)
-                this.postsUpdated.next([...this.posts])
+  getPosts(): void {
+    this.http.get<Post[]>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postsData: Post[]) => {
+          return postsData.map((post: any): Post => {
+            return {
+              title: post.title as string,
+              content: post.content as string,
+              id: post._id as string
             }
-        )
+          })
+        })
+      )
+      .subscribe((postData: Post[]): void => {
+        this.posts = postData
+        this.postsUpdated.next([...this.posts])
+      })
+  }
 
-    }
+  getPost(id: string): Observable<Post> {
+    return this.http.get<Post>(BASE_URI + POST_ENDPOINT + `/${id}`)
+  }
 
-    updatePost(id: string, title: string, content: string): void {
-        const post: Post = {id, title, content}
-        this.http.put(BASE_URI + POST_ENDPOINT + `/${id}`, post)
-            .subscribe(response => {
-                //? const updatedPosts = [...this.posts];
-                //? const oldPostIndex = updatedPosts.findIndex(p=>p.id === id)
-                //? updatedPosts[oldPostIndex] = post
-                //? this.posts = updatedPosts
-                //? this.postsUpdated.next([...this.posts])
-            })
+  addPost(inPost: Partial<Post>): void {
+    const post: Post = {
+      id: null,
+      title: inPost.title as string,
+      content: inPost.content as string
     }
+    this.http.post<PostResponseDto>(BASE_URI + POST_ENDPOINT, post).subscribe(
+      (responseData: PostResponseDto): void => {
+        post.id = responseData.postId
+        this.posts.push(post)
+        this.postsUpdated.next([...this.posts])
+      }
+    )
 
-    deletePost(postId: string): void {
-        this.http.delete(BASE_URI + POST_ENDPOINT + `/${postId}`)
-            .subscribe((): void => {
-                    this.posts = this.posts.filter(post => post.id !== postId)
-                    this.postsUpdated.next([...this.posts])
-                }
-            )
-    }
+  }
 
-    getPostUpdateListener(): Observable<Post[]> {
-        return this.postsUpdated.asObservable()
-    }
+  updatePost(id: string, title: string, content: string): void {
+    const post: Post = {id, title, content}
+    this.http.put<{ message: string }>(BASE_URI + POST_ENDPOINT + `/${id}`, post)
+      .subscribe(
+        (response: { message: string }): void => {
+          this.reloadUpdatedPost(post, id)
+        })
+  }
+
+  reloadUpdatedPost(post: Post, id: string): void {
+    const updatedPosts: Post[] = [...this.posts];
+    const oldPostIndex: number = updatedPosts.findIndex((p: Post): boolean => p.id === id)
+    updatedPosts[oldPostIndex] = post
+    this.posts = updatedPosts
+    this.postsUpdated.next([...this.posts])
+  }
+
+  deletePost(postId: string): void {
+    this.http.delete(BASE_URI + POST_ENDPOINT + `/${postId}`)
+      .subscribe((): void => {
+          this.posts = this.posts.filter(post => post.id !== postId)
+          this.postsUpdated.next([...this.posts])
+        }
+      )
+  }
+
+  getPostUpdateListener(): Observable<Post[]> {
+    return this.postsUpdated.asObservable()
+  }
 }
