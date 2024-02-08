4f568f4eb441faf2b2b8daab17e60342ee0ad5a0
Juri <lubonjajurgen1@gmail.com>
2024-02-08 14:31:06 +0100

    image uplad button added Stylization fixes

--- mean-course-2/src/app/app.module.ts
+++ mean-course-2/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {MatAnchor, MatButton, MatIconButton} from "@angular/material/button";
+import {MatAnchor, MatButton, MatFabButton, MatIconButton} from "@angular/material/button";
 import { HeaderComponent } from './header/header.component';
 import {MatToolbar} from "@angular/material/toolbar";
 import {
@@ -31,30 +31,31 @@ import {MatProgressSpinner} from "@angular/material/progress-spinner";
     HeaderComponent,
     PostListComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        MatCard,
-        MatFormField,
-        MatLabel,
-        MatInput,
-        MatButton,
-        MatToolbar,
-        MatAccordion,
-        MatExpansionPanel,
-        MatExpansionPanelHeader,
-        MatDivider,
-        MatCardContent,
-        MatError,
-        MatExpansionPanelActionRow,
-        MatIconButton,
-        MatIcon,
-        MatCardTitle,
-        HttpClientModule,
-        MatAnchor,
-        MatProgressSpinner
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatCard,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton,
+    MatToolbar,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatDivider,
+    MatCardContent,
+    MatError,
+    MatExpansionPanelActionRow,
+    MatIconButton,
+    MatIcon,
+    MatCardTitle,
+    HttpClientModule,
+    MatAnchor,
+    MatProgressSpinner,
+    MatFabButton
+  ],
   providers: [
     provideAnimationsAsync()
   ],
