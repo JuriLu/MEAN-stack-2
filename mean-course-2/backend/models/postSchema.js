6d46a57ff50b12932317e677309b78c38b75dbeb
Juri <lubonjajurgen1@gmail.com>
2024-02-09 18:54:01 +0100

    Image added is Post Schema Image managed in express backend, also post request sends back object as a response

--- mean-course-2/backend/models/postSchema.js
+++ mean-course-2/backend/models/postSchema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const postSchema = mongoose.Schema({
   //* title: String   CAN LEAVE ALSO LIKE THIS
   title: {type: String, required: true},
-  content: {type: String, required: true}
+  content: {type: String, required: true},
+  imagePath: {type: String, required: true},
 })
 
 module.exports =  mongoose.model('Post',postSchema)
--- mean-course-2/backend/routes/postsRoute.js
+++ mean-course-2/backend/routes/postsRoute.js
@@ -1,24 +1,55 @@
 const express = require("express")
+const multer = require('multer')
 const Post = require('../models/postSchema')
 
 const EXPRESS_APP_ROUTER = express.Router();
 
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+}
+
+//? Image management
+const storage = multer.diskStorage({
+  //? destination runs when multer saves a file
+  destination: (request, file, callback) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype]
+    let error = new Error("Invalid mime type")
+    if (isValid) {
+      error = null
+    }
+    callback(error, "backend/images",)    //* 1st argument is if detected an error(null), 2nd is a string with the path where is will be stored (relative to server.js file)
+  },
+  filename: (request, file, callback) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-')
+    const extension = MIME_TYPE_MAP[file.mimetype]
+    callback(null, name + '-' + Date.now() + '.' + extension)
+  }
+})
 
 //? POST
-EXPRESS_APP_ROUTER.post('', (
+EXPRESS_APP_ROUTER.post('', multer({storage}).single('image'), (
   request,
   response,
   next
 ) => {
+  const serverUrl = request.protocol + '://' + request.get('host')
   const post = new Post({
     title: request.body.title,
     content: request.body.content,
+    imagePath: serverUrl + '/images/' + request.file.filename
   })
   //! SAVES DATA TO DATABASE
   post.save().then(createdPost => {
     response.status(201).json({
       message: 'Post added successfuly',
-      postId: createdPost._id
+      post: {
+        id: createdPost._id,
+        title: createdPost.title,
+        content: createdPost.content,
+        imagePath: createdPost.imagePath,
+      }
     })
   })
 })
@@ -59,7 +90,7 @@ EXPRESS_APP_ROUTER.get('', (
 })
 
 //? DELETE
-EXPRESS_APP_ROUTER.delete( '/:id', (
+EXPRESS_APP_ROUTER.delete('/:id', (
   request,
   response,
   next) => {
